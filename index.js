2dea4e8563dcfcc32bec6e54f933990f36f58303
God'swill Sekav Apya <godswillapya@gmail.com>
2025-11-24 01:59:54 +0100

    feat: add decrement button and functionality to update count

--- index.js
+++ index.js
@@ -10,4 +10,9 @@ function increment() {
   return console.log(count);
 }
 
+function decrement() {
+    count = count - 1;
+    document.getElementById("count-el").innerText = count;
+}
+
 console.log(increment());
