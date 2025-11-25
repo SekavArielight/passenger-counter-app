77adcc48e5d00c3ab57020ab0296defe621beded
God'swill Sekav Apya <godswillapya@gmail.com>
2025-11-25 03:47:32 +0100

    feat: add welcome message functionality and update script reference

---
+++ test2.js
@@ -0,0 +1,13 @@
+// Grab the welcome-el paragraph and store it in a variable called welcomeEl
+
+// Create two variables (name & greeting) that contains your name
+// and the greeting we want to render on the page
+
+// Render the welcome message using welcomeEl.innerText
+
+const welcomeEl = document.getElementById("count-el");
+
+let name = "God'swill Sekav Apya";
+let greeting = "Hi friends, I am ";
+
+welcomeEl.innerText = greeting + name;
