95b1ba9913c56a37a3f216849fa7a257e496a1f2
God'swill Sekav Apya <godswillapya@gmail.com>
2025-11-25 22:51:56 +0100

    fix: correct syntax and add save element reference for clarity

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let count = 0;
-let countEl = document.getElementById("count-el")
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
 
 function increment() {
   count += 1;
@@ -7,8 +8,10 @@ function increment() {
   console.log(count);
 }
 
-function save(){
-    console.log(count)
+function save() {
+  saveVariable = count + "-"
+  saveEl.innerText += saveVariable;
+  console.log(count);
 }
 
 // function decrement() {
